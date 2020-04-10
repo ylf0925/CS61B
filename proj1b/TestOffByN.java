9ae51d2c48f6d9c226d6d6eefc705093ee157850
lone_fade <30333190+ylf0925@users.noreply.github.com>
2020-04-10 15:59:26 +0800

    proj1b final commit

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -3,14 +3,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByN {
-    static CharacterComparator obn3 = new OffByN(3);
+    static CharacterComparator obn5 = new OffByN(5);
 
     @Test
     public void testOffByN() {
-        assertTrue(obn3.equalChars('a', 'd'));
-        assertTrue(obn3.equalChars('b', 'e'));
-        assertFalse(obn3.equalChars('a', 'a'));
-        assertFalse(obn3.equalChars('b', 'c'));
+        assertTrue(obn5.equalChars('a', 'f'));
+        assertTrue(obn5.equalChars('b', 'g'));
+        assertFalse(obn5.equalChars('a', 'a'));
+        assertFalse(obn5.equalChars('b', 'c'));
     }
 }
 
