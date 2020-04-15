fa99c436900214a859da0763a72ae441d6b16805
lone_fade <30333190+ylf0925@users.noreply.github.com>
2020-04-15 12:46:07 +0800

    hw1 delete extra code

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -1,13 +1,17 @@
 package synthesizer;
 
-public abstract class AbstractBoundedQueue<T> implements BoundedQueue{
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     protected int fillCount;
     protected int capacity;
-    public abstract int capacity();
-    public abstract int fillCount();
-    public abstract boolean isEmpty();
-    public abstract boolean isFull();
-    public abstract T peek();
-    public abstract T dequeue();
-    public abstract void enqueue(Object x);
+
+    @Override
+    public int capacity() {
+        return capacity;
+    }
+
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+
 }
